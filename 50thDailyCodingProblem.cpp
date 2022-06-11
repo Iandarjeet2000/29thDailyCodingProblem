44a47c0aa0d73c4ea5ded2293ed4b54bc34b7c01
Iandar Jeet Kumar Singh <iandarjeetsingh@gmail.com>
2022-06-11 22:59:45 +0530

    Add files via upload

---
+++ 50thDailyCodingProblem.cpp
@@ -0,0 +1,82 @@
+/*Suppose an arithmetic expression is given as a binary tree. Each leaf is an integer and each internal node is one of '+', '−', '∗', or '/'.
+
+Given the root to such a tree, write a function to evaluate it.
+
+For example, given the following tree:
+
+    *
+   / \
+  +    +
+ / \  / \
+3  2  4  5
+You should return 45, as it is (3 + 2) * (4 + 5).*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node {
+    string data;
+    struct node *left;
+    struct node *right;
+
+    node(string x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+bool isExpression(string &s) {
+    if (s == "+" or s == "-" or s == "*" or s == "/") return true;
+    return false;
+}
+
+
+class Solution{
+  public:
+    int evalTree(node* root) {
+         if(root==NULL)
+            return 0;
+        int l = evalTree(root->left);
+        int r = evalTree(root->right);
+        
+        if(root->data=="*")return l*r;
+        else if(root->data=="+")return l+r;
+        else if(root->data=="/")return l/r;
+        else if(root->data=="-")return l-r;
+        else return stoi(root->data);
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s[n];
+        for (int i = 0; i < n; i++) cin >> s[i];
+
+        int p = 0;
+        node *root = new node(s[p++]);
+        queue<node *> q;
+        q.push(root);
+        while (!q.empty()) {
+            node *f = q.front();
+            q.pop();
+            node *l, *r;
+            if (isExpression(f->data)) {
+                l = new node(s[p++]);
+                r = new node(s[p++]);
+                f->left = l;
+                f->right = r;
+                q.push(l);
+                q.push(r);
+            }
+        }
+        Solution obj;
+        cout << obj.evalTree(root) << endl;
+    }
+}
